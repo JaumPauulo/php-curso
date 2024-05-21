df0bf1a4b377024fe7dc3f1be841b131b4deeeb8
João Paulo <joaopaulodasilva506@gmail.com>
2024-05-21 11:11:09 -0300

    delete

--- Aula01/arquivo-01.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//função para realizar um include ou require para teste;
-function somar($a,$b){
-    return $a + $b;
-}
-
-?>
--- Aula01/arquivo-02.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require "arquivo-02.php";
-
-$resultado = somar(10,25);
-echo $resultado;
-echo ("oi");
-
-?>
