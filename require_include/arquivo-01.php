a31affa169220d807ff5cdfce11870428f1eab20
João Paulo <joaopaulodasilva506@gmail.com>
2024-05-21 10:58:16 -0300

    Aula Sobre require,include e suas diferenças/como usar

---
+++ require_include/arquivo-01.php
@@ -0,0 +1,7 @@
+<?php
+
+//função para realizar um include ou require para teste;
+function somar($a, $b)
+{
+    return $a + $b;
+}
---
+++ require_include/arquivo-02.php
@@ -0,0 +1,6 @@
+<?php
+
+require "arquivo-01.php";
+
+$resultado = somar(10, 25);
+echo $resultado;
